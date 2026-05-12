2f57f136e3a39dc13456332ea9dffd854916e845
MD-Developer-yt <animeworldday2005@gmail.com>
2026-05-12 18:53:21 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,55 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+import os
+
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+MONGO_URI = os.environ.get(
+    "MONGO_URI",
+    ""
+)
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+LOG_CHANNEL = int(
+    os.environ.get(
+        "LOG_CHANNEL",
+        "-1003608036510"
+    )
+)
+
+FORCE_SUB = "Anime_UpdatesAU"
+
+ADMINS = [
+    int(x)
+    for x in os.environ.get(
+        "ADMINS",
+        "7284759394"
+    ).split()
+]
+
+OMDB_API = os.environ.get(
+    "OMDB_API",
+    ""
+)
+
+STREAM_URL = os.environ.get(
+    "STREAM_URL",
+    ""
+)
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
